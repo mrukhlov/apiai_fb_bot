84360d6261eed92a343acc3addb059cd2f5dd52e
mrukhlov <mrukhlov@speaktoit.com>
2016-08-30 18:40:16 +0300

    'aaa'

--- src/app.js
+++ src/app.js
@@ -121,6 +121,44 @@ function chunkString(s, len) {
     return output;
 }
 
+const message_gen = {
+    attachment: {
+        type: "template",
+            payload: {
+            template_type: "generic",
+                elements: [{
+                title: "rift",
+                subtitle: "Next-generation virtual reality",
+                item_url: "https://www.oculus.com/en-us/rift/",
+                image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                buttons: [{
+                    type: "web_url",
+                    url: "https://www.oculus.com/en-us/rift/",
+                    title: "Open Web URL"
+                }, {
+                    type: "postback",
+                    title: "Call Postback",
+                    payload: "Payload for first bubble",
+                }],
+            }, {
+                title: "touch",
+                subtitle: "Your Hands, Now in VR",
+                item_url: "https://www.oculus.com/en-us/touch/",
+                image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                buttons: [{
+                    type: "web_url",
+                    url: "https://www.oculus.com/en-us/touch/",
+                    title: "Open Web URL"
+                }, {
+                    type: "postback",
+                    title: "Call Postback",
+                    payload: "Payload for second bubble",
+                }]
+            }]
+        }
+    }
+}
+
 function sendFBMessage(sender, messageData, callback) {
     console.log(sender);
     request({
@@ -129,7 +167,8 @@ function sendFBMessage(sender, messageData, callback) {
         method: 'POST',
         json: {
             recipient: {id: sender},
-            message: messageData
+            //message: messageData
+            message: message_gen
         }
     }, (error, response, body) => {
         if (error) {

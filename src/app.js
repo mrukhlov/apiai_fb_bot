2d33579a8c563a492db390cfa98dd2747f1efbbc
mrukhlov <mrukhlov@speaktoit.com>
2016-09-01 11:23:54 +0300

    'webhook_test'

--- src/app.js
+++ src/app.js
@@ -301,6 +301,8 @@ app.post('/webhook/', (req, res) => {
 
 app.post('/webhook_apiai/', (req, res) => {
 
+    var weather_query = false;
+
     const generic_message = {
         attachment: {
             type: "template",
@@ -336,6 +338,7 @@ app.post('/webhook_apiai/', (req, res) => {
                         generic_message.attachment.payload.elements[0].image_url = "http://www.cbc.ca/inthekitchen/assets_c/2012/11/MargheritaPizza21-thumb-596x350-247022.jpg";
                 }
             case 'show_weather':
+                weather_query = true;
                 if(data.result.parameters['geo-city']){
                     var city = data.result.parameters['geo-city'];
                     var base_url = "https://query.yahooapis.com/v1/public/yql?" + "q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27"+city+"%27%29" + "&format=json";
@@ -345,6 +348,10 @@ app.post('/webhook_apiai/', (req, res) => {
                     }, function(error, response, body){
                         if(error) {
                             console.log(error);
+                            return res.status(400).json({
+                                status: "error",
+                                error: err
+                            });
                         } else {
                             console.log('here');
                             //console.log(response.statusCode, body);
@@ -362,15 +369,22 @@ app.post('/webhook_apiai/', (req, res) => {
                             generic_message.attachment.payload.elements[0].item_url = channel.link;
                             generic_message.attachment.payload.elements[0].image_url = channel.image.url;
                             generic_message.attachment.payload.elements[0].buttons[0].url = channel.link;
+                            return res.status(200).json({
+                                data: {
+                                    facebook: generic_message
+                                }
+                            })
                         }
                     });
                 }
         }
-        return res.status(200).json({
-            data: {
-                facebook: generic_message
-            }
-        })
+        if (weather_query== false){
+            return res.status(200).json({
+                data: {
+                    facebook: generic_message
+                }
+            })
+        }
     } catch (err) {
         return res.status(400).json({
             status: "error",

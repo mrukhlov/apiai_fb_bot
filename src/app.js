9e39dafe484a6dd45dc6b6a3a1a94e11ed4d2678
mrukhlov <mrukhlov@speaktoit.com>
2016-09-01 18:11:11 +0300

    'webhook_test'

--- src/app.js
+++ src/app.js
@@ -454,7 +454,7 @@ app.post('/webhook_apiai/', (req, res) => {
                 }
                 break;
         }*/
-        console/log(weather_query);
+        console.log(weather_query);
         if (weather_query != true) {
             return res.status(200).json({
                 data: {
